bb779075be7c009ef41b4c2c84a6f29066ef20b3
Emory Puches <epuches@gmail.com>
2020-08-11 07:30:47 -0400

    modified:   bot.py 	new file:   config.py

--- bot.py
+++ bot.py
@@ -4,15 +4,6 @@ from discord.ext import commands
 from discord.utils import get
 from collections import defaultdict
 
-api_key = "YOUR_KEY"
-api_secret = "YOUR_SECRET"
-access_token = "NzIzNDk2MTc1OTY5NTAxMjU2.XvNP4g.ef9lrx9rsM_FI3Mlr_YYR-eT6Ns"
-token_secret = "YOUR_TOKEN_SECRET"
-#trades_channel_id = 723538942095392778
-trades_channel_id = 724708986032488459
-bot_id = 723496175969501256
-
-
 client = discord.Client()
 client = commands.Bot('!')
 
@@ -246,5 +237,3 @@ async def on_ready():
 
 
 client.run(config.access_token)    # Bot token
-
-input()
---
+++ config.py
@@ -0,0 +1,7 @@
+api_key = "YOUR_KEY"
+api_secret = "YOUR_SECRET"
+access_token = "NzIzNDk2MTc1OTY5NTAxMjU2.XvNP4g.ef9lrx9rsM_FI3Mlr_YYR-eT6Ns"
+token_secret = "YOUR_TOKEN_SECRET"
+#trades_channel_id = 723538942095392778
+trades_channel_id = 724708986032488459
+bot_id = 723496175969501256
